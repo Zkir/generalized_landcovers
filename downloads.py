9dd654ab663f58db1fcfdbfcfb2aaf0b16f457bd
Zkir <Mekillot@yandex.ru>
2024-02-07 17:40:24 +0300

    last_know_edit_date added

--- downloads.py
+++ downloads.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 from zwebpage import ZWebPage
+import psycopg2
 
 LOCAL_PATH="data/export/downloads"
 WEB_PATH="downloads"
@@ -13,13 +14,31 @@ files=[[LANDCOVERS_ZIP,"Generalized landcovers polygons in shape format"],
          [PEAKS_ZIP," Mountain peak points with cartographic importance calculated  <br />using the Discrete Isolation method."],
          [PLACES_ZIP,"City and town points with cartographic importance calculated <br />using the Grid Cell method."] ]
 
+
+db_user_name = os.environ.get("PGUSER")
+db_user_password = os.environ.get("PGPASSWORD")
+
+if db_user_name is None or db_user_password is None:
+    print('Error: Postgres user name or password is not set, exiting')
+    print('Check PGUSER and PGPASSWORD environment variables')
+    exit(1)
+
+conn = psycopg2.connect(dbname='gis', user=db_user_name,
+                        password=db_user_password, host='localhost')
+cursor = conn.cursor()
+cursor.execute("select value as last_known_edit  from osm2pgsql_properties where property='current_timestamp';" )
+records = cursor.fetchall()
+last_known_edit=str(records[0][0])
+
+
 page = ZWebPage("downloads.html", "OpenLandcoverMap Downloads")
 page.print("""<h1>OpenLandcoverMap downloads</h1>
     <table class="sortable" >
     <tr>
     <th>File</th>
     <th>Description</th>
-    <th>Update date</th>
+    <th> Update date </th>
+    <th>Last known edit</th>
     </tr>""" )
 
 for file in files:
@@ -34,11 +53,11 @@ for file in files:
     s+='  <td> '
     s+=   datetime.fromtimestamp(os.path.getmtime(LOCAL_PATH+'/'+file[0])).strftime('%Y-%m-%d')
     s+='  </td>'
+    s+='  <td>'+last_known_edit+'</td>'
+
     s+='</tr>'
     page.print(s)
 
 page.print('</table>')
-
-
 
 page.write()
