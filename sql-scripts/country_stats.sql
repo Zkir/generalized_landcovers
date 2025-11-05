fe07b7d7c07a15752d4247f370fbe5a1b5712d7b
Zkir <Mekillot@yandex.ru>
2025-11-05 13:03:31 +0300

    country stats improved

--- sql-scripts/country_stats.sql
+++ sql-scripts/country_stats.sql
@@ -1,9 +1,5 @@
 SET statement_timeout = 0;
 
--- drop table if exists h3.country_polygons;
--- drop table if exists h3.country_stats;
--- drop table if exists h3.country_stats2;
-
 /*
 CREATE TABLE h3.country_polygons AS 
     SELECT osm_id, "name", tags->'name:en' AS name_en, tags->'ISO3166-1' as ISO3166, admin_level, tags, way AS geom 
@@ -13,8 +9,10 @@ CREATE TABLE h3.country_polygons AS
         
 --SELECT * FROM   h3.ne_10m_admin_0_countries ORDER BY admin;      
 
+/* ------------------------------------------------------------------------------
+ *  h3.country_polygons: just country polygons in the proper coordinate system
+ * ------------------------------------------------------------------------------*/
 DROP TABLE IF EXISTS h3.country_polygons;
-
 CREATE TABLE h3.country_polygons AS         
     SELECT 
         name_en AS name_en, adm0_a3 AS ISO3166,
@@ -27,8 +25,30 @@ CREATE TABLE h3.country_polygons AS
 CREATE INDEX ix_country_polygons  ON h3.country_polygons (name_en);
 CREATE INDEX gix_country_polygons ON h3.country_polygons USING GIST (geom);
 
-DROP TABLE IF EXISTS h3.country_stats;
+/*------------------------------------------------------------------------------
+ * h3.no_landcover_per_country
+ * Note that we cannot just assign coutry id to the hex, 
+ * because hex can belong to several countries (country borders are not hex borders)
+ *  we will use it for EMPTY HEX INSPECTOR feature
+ * ------------------------------------------------------------------------------*/
+DROP TABLE IF EXISTS h3.no_landcover_per_country;
+CREATE TABLE h3.no_landcover_per_country AS
+    SELECT
+      n.ix AS ix,    
+      n.geom AS geom,
+      c.name_en AS country_name
+    FROM
+      h3.no_landcover n
+      JOIN h3.country_polygons c ON ST_Intersects (n.geom, c.geom);
 
+CREATE INDEX ix_no_landcover_per_country  ON h3.country_polygons (ix, country_name);
+
+/*------------------------------------------------------------------------------
+ *  h3.country_stats
+ *  landcover statistics per country
+ * ------------------------------------------------------------------------------*/
+
+DROP TABLE IF EXISTS h3.country_stats;
 CREATE TABLE h3.country_stats AS
     SELECT
       cp.name_en,
@@ -49,13 +69,12 @@ CREATE TABLE h3.country_stats AS
           ) as th ON cp.name_en = th.name_en
       LEFT JOIN(
             SELECT
-              c.name_en,
+              n.country_name AS name_en,
               count(n.ix) AS empty_hexes
             FROM
-              h3.no_landcover n
-              JOIN h3.country_polygons c ON ST_Intersects (n.geom, c.geom)
+              h3.no_landcover_per_country n
             GROUP BY
-              c.name_en
+              c.country_name
           ) as eh ON cp.name_en = eh.name_en
       LEFT JOIN (
             SELECT
